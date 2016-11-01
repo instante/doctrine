95e61af409b508b1d423b1a43be130f156d41de1
Richard Ejem <richard@ejem.cz>
2016-11-01 16:53:52 +0100

    removed unnecessary pushd call

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
-pushd "$(dirname "$0")/.."
+#!/usr/bin/env sh
+cd "$(dirname "$0")/.."
 
 cp ./tests/php-unix.ini ./tests/php.ini
 PHP_EXT=`php -r "echo ini_get('extension_dir');"`
@@ -7,8 +7,5 @@ echo "" >> ./tests/php.ini # empty line
 echo "extension_dir=$PHP_EXT" >> ./tests/php.ini
 
 ./vendor/bin/tester ./tests/$1 -p php -c ./tests
-EXITCODE=$?
 
-popd
-
-exit "$EXITCODE"
+exit "$?"
